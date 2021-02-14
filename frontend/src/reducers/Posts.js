891316abc8a46e59a1af05bccc54c2a36a24c938
shemaM <azertshema@gmail.com>
2021-02-14 16:47:24 +0200

    redux setup

---
+++ frontend/src/reducers/Posts.js
@@ -0,0 +1,10 @@
+export const reducers = (posts = [], action) => {
+  switch (action.type) {
+    case 'FETCH_All':
+      return posts;
+    case 'CREATE':
+      return posts;
+    default:
+      return posts;
+  }
+};
---
+++ frontend/src/reducers/index.js
@@ -0,0 +1,4 @@
+import { combineReducers } from 'redux';
+import { reducers } from './Posts';
+
+export default combineReducers({ reducers });
